4957d5cfbebf4e122fee533bef644fca51685255
Gaurav <parmargaurav1234@gmail.com>
2021-09-27 15:55:08 +0530

    eighth

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,21 @@
 const mongoose = require('mongoose');
+
+//user model
+const userSchema = new mongoose.Schema({
+    first_name: {type: String, required: true},
+    last_name: { type:String, required: true},
+    email: {type: String, required: true},
+    password: {type:String, required: false},
+    profile_photo_url: {type: String, required:true},
+    roles: {type:String, required:true},
+   
+},
+{
+    versionKey:false,
+    timestamps:true
+    
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
