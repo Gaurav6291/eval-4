2422ff93a42308e44b025be4b7282603ae26ce10
Gaurav <parmargaurav1234@gmail.com>
2021-09-27 15:17:01 +0530

    fifth

--- server.js
+++ server.js
@@ -2,8 +2,34 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const connect = () => {
-    return mongoose.connect("mongodb://127.0.0.1:27017/test")
+    return mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
+
+//user model
+const userSchema = new mongoose.Schema({
+    first_name: {type: String, required: true},
+    last_name: { type:String, required: true},
+    email: {type: String, required: true},
+    password: {type:String, required: false},
+    profile_photo_url: {type: String, required:true},
+    roles: {type:String, required:true}
+})
+
+const User = mongoose.model("user", userSchema)
+
+
+//student model
+const studentSchema = new mongoose.Schema({
+    roll_no: {type: Number, required: true},
+    batch: { type:String, required: true},
+   user: { 
+       type: mongoose.Schema.Types.ObjectId,
+       ref:"user",
+       require:"true"
+ }
+})
+
+const User = mongoose.model("user", userSchema)
 
 const app = express();
 
